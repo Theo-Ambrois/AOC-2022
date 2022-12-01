3b051c95f8546b997606f76e49ad297eea22a895
unknown <ambrois.theo@gmail.com>
2022-12-01 18:32:50 +0100

    AOC1

---
+++ AOC1/AOC1/Elf.cs
@@ -0,0 +1,43 @@
+﻿namespace AOC1
+{
+    public class Elf
+    {
+        public List<int> CalculateCals()
+        {
+            int cal = 0;
+            List<int> cals = new List<int>();
+
+            foreach (string line in System.IO.File.ReadLines(@"D:\AoC\AOC-2022\AOC1\AOC1\elf.txt"))
+            {
+                if (line != string.Empty)
+                {
+                    cal =+ Int32.Parse(line);
+                }
+                else if (line == string.Empty)
+                {
+                    cals.Add(cal);
+                    cal = 0;
+                }
+            }        
+            return cals;
+        }
+
+        public int SortMaxCal(int numberOfElfs)
+        {
+            int sum = 0;
+            List<int> cals = new List<int>();
+            cals = CalculateCals();
+
+            for(int i = 0; i < numberOfElfs; i++)
+            {
+                int max = cals.Max();
+                Console.WriteLine(max.ToString());
+                sum =+ max;
+                cals.Remove(max);
+            }
+
+            Console.WriteLine(sum.ToString());
+            return sum;
+        }
+    }
+}
---
+++ AOC1/AOC1/Program.cs
@@ -0,0 +1,17 @@
+﻿using AOC1;
+
+class Program
+{
+
+    static public void Main()
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+        Elf elf = new Elf();
+        elf.SortMaxCal(3);
+
+        watch.Stop();
+
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+}
