587782bad854247c80e75a9ed4efbd9887e6b3f6
unknown <ambrois.theo@gmail.com>
2022-12-06 22:34:47 +0100

    jour 4

---
+++ AOC/AOC4/AOC4.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace AOC4
+{
+    public class AOC4
+    {
+        public int amountOfPairs()
+        {
+
+            var assignedSections = File.ReadLines(@"D:\AoC\AOC-2022\AOC\AOC4\input.txt").ToList();
+            var sum = 0;
+            assignedSections.ForEach(pair =>
+            {
+                var sectionRanges = ParseSections(pair).ToImmutableArray();
+                if (IsOverlapping(sectionRanges[0], sectionRanges[1]))
+                {
+                    sum++;
+                }
+            });
+
+            Console.WriteLine(sum.ToString());
+            //return sum;
+
+            sum = 0;
+
+            assignedSections.ForEach(pair =>
+            {
+                var sectionRanges = ParseSections(pair).ToImmutableArray();
+                if (IsIntersecting(sectionRanges[0], sectionRanges[1])) sum++;
+            });
+
+            Console.WriteLine(sum.ToString());
+            return sum;
+
+        }
+
+        IEnumerable<Range> ParseSections(string assignmentPair)
+        {
+            return assignmentPair.Split(',').Select(GetRange).ToList();
+        }
+
+        Range GetRange(string assignmentPair)
+        {
+            var sections = assignmentPair.Split('-').Select(int.Parse).ToArray();
+            return new Range(sections[0], sections[1]);
+        }
+
+        bool IsOverlapping(Range startRange, Range endRange)
+        {
+            return (startRange.Start.Value <= endRange.Start.Value && startRange.End.Value >= endRange.End.Value) || (endRange.Start.Value <= startRange.Start.Value && endRange.End.Value >= startRange.End.Value);
+        }
+
+        bool IsIntersecting(Range startRange, Range endRange)
+        {
+            return startRange.Start.Value <= endRange.End.Value && endRange.Start.Value <= startRange.End.Value;
+        }
+
+    }
+}
--- AOC/TestProject/Program.cs
+++ AOC/TestProject/Program.cs
@@ -7,10 +7,12 @@
         AOC1.AOC1 AOC1 = new AOC1.AOC1();
         AOC2.AOC2 AOC2 = new AOC2.AOC2();
         AOC3.AOC3 AOC3 = new AOC3.AOC3();
+        AOC4.AOC4 AOC4 = new AOC4.AOC4();
         //AOC1.SortMaxCal(3);
         //AOC2.CalculateScoreMax();
-        AOC3.CalculteSum();
-        AOC3.CalculateSumThreeElfGroup();
+        //AOC3.CalculteSum();
+        //AOC3.CalculateSumThreeElfGroup();
+        AOC4.amountOfPairs();
 
         watch.Stop();
 
